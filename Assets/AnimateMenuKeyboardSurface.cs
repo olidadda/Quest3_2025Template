e25bb92805c997bf23ee2a758e3e0aebfe025993
OLIPC_WIN11\olida <olidadda@gmail.com>
2025-03-19 19:59:33 +0100

    Made Additive scenes, animation (not yet tested) for shrinking menu keyboard surface, now for rewrite load script

---
+++ Assets/AnimateMenuKeyboardSurface.cs
@@ -0,0 +1,17 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AnimateMenuKeyboardSurface : MonoBehaviour
+{
+    [SerializeField] float timeToShrink = 1.0f;
+    public void ShrinkAndDisable()
+    {
+        // Start the scaling animation
+        transform.DOScale(Vector3.zero,timeToShrink) // Scale to zero over 1 second
+            .OnComplete(() => gameObject.SetActive(false)); // Disable the GameObject once scaling is complete
+    }
+}
+
