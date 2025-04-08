5c0c461f43d2f7d9fc5af7a95ae051b9cc93ab60
OLIPC_WIN11\olida <olidadda@gmail.com>
2025-04-08 04:32:56 +0200

    First apk build of partial game attempted

--- Assets/FadeScreenAsset/InstructionsFadeScreen.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InstructionsFadeScreen 
-{
-    
-    //Place the Fade Screen prefab on the central tracking object and then activate through scene loader
-}
